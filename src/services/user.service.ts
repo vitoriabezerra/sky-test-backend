79f25d256b19a84bc7f9fef94907313b72f47331
Vitória Bezerra <56508704+vitoriabezerra@users.noreply.github.com>
2024-03-04 12:00:40 -0300

    encript user password

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,21 @@
 import User, { IUser } from "../models/user.model";
 import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+
+// number of salts for hashing
+const saltRounds = 10;
+
+async function hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    return bcrypt.hash(password, salt);
+}
 
 export async function createUser(user: IUser) {
     try {
         // Check if the e-mail is new
         const existingUser = await User.findOne({ email: user.email });
+        const passwordHash = await hashPassword(user.senha);
         const teste: IUser = {
             ...user,
             id: uuidv4(),
